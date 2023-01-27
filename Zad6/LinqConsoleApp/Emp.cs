8b224da07e8e42d5eeab741f51f78d54a96ed30e
s21172 <s21172@pjwstk.edu.pl>
2023-01-27 20:17:08 +0100

    LINQ

---
+++ Zad6/LinqConsoleApp/Emp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LinqConsoleApp
+{
+    public class Emp
+    {
+        public int Empno { get; set; }
+        public string Ename { get; set; }
+        public string Job { get; set; }
+        public int Salary { get; set; }
+        public DateTime? HireDate { get; set; }
+        public int? Deptno { get; set; }
+        public Emp Mgr { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Empno}\t{Ename}\t{Job}\t{Salary}\t{HireDate}\t{Deptno}\t{Mgr?.Empno}";
+        }
+    }
+}
---
+++ Zad6/LinqConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqConsoleApp
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Emps:");
+            LinqSamples.Emps.ToList().ForEach(Console.WriteLine);
+            Console.WriteLine();
+            Console.WriteLine("Depts:");
+            LinqSamples.Depts.ToList().ForEach(Console.WriteLine);
+            LinqSamples.Przyklad1();
+            LinqSamples.Przyklad2();
+            LinqSamples.Przyklad3();
+            LinqSamples.Przyklad4();
+            LinqSamples.Przyklad5();
+            LinqSamples.Przyklad6();
+            LinqSamples.Przyklad7();
+            LinqSamples.Przyklad8();
+            LinqSamples.Przyklad9();
+            LinqSamples.Przyklad10();
+            LinqSamples.Przyklad11();
+            LinqSamples.Przyklad12();
+        }
+    }
+}
